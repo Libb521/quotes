fb892fc207ed663630baf07c9c59fc4d81a5969e
Libb521 <eotieno39@yahoo.com>
2020-01-12 13:55:47 +0300

    i nested the components and made them available as a child component of the root component, i then added the app-quote to the app.component.html in order for the nesting to be complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,15 +7,5 @@ import {  Quotes } from './quotes';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
- 
-  quotes:Quotes = [
-    {id:1, name:'If you are always trying to be normal you will never know how amazing you can be', author:'Maya Angelou'},
-    {id:2, name:'Tears are words that need to be written', author:'Paulo Coelho'},
-    {id:3, name:'Writing books is the closest men ever come to childbearing', author:'Norman Mailer'},
-  ];
 
-  constructor() {}
-  ngOnInit() {
-    
-  }
   }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quotes } from '../quotes';
 
 @Component({
   selector: 'app-quote',
@@ -7,6 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteComponent implements OnInit {
 
+  quotes:Quotes = [
+    {id:1, name:'If you are always trying to be normal you will never know how amazing you can be', author:'Maya Angelou'},
+    {id:2, name:'Tears are words that need to be written', author:'Paulo Coelho'},
+    {id:3, name:'Writing books is the closest men ever come to childbearing', author:'Norman Mailer'},
+  ];
   constructor() { }
 
   ngOnInit() {
