ee4d1f510bf6946a6b334aba8f7854e1631733fa
Libb521 <eotieno39@yahoo.com>
2020-01-12 14:08:22 +0300

    i created a quote child component called 'quote-detail', this will enable me display the description of each quote

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,9 +9,9 @@ import { Quotes } from '../quotes';
 export class QuoteComponent implements OnInit {
 
   quotes:Quotes = [
-    {id:1, name:'If you are always trying to be normal you will never know how amazing you can be', author:'Maya Angelou'},
-    {id:2, name:'Tears are words that need to be written', author:'Paulo Coelho'},
-    {id:3, name:'Writing books is the closest men ever come to childbearing', author:'Norman Mailer'},
+    {id:1, name:'If you are always trying to be normal you will never know how amazing you can be', description:'Maya Angelou'},
+    {id:2, name:'Tears are words that need to be written', description:'Paulo Coelho'},
+    {id:3, name:'Writing books is the closest men ever come to childbearing', description:'Norman Mailer'},
   ];
   constructor() { }
 
