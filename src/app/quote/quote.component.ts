0e176a7b880cad1c93a8148e54b0916d54ca8968
Libb521 <eotieno39@yahoo.com>
2020-01-13 10:59:08 +0300

    i added a date count pipe that will help count the number of days

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,9 +9,9 @@ import { Quotes } from '../quotes';
 export class QuoteComponent implements OnInit {
 
   quotes:Quotes [] = [
-    new Quotes(1, '"If you are always trying to be normal you will never know how amazing you can be"', 'Maya Angelou'),
-    new Quotes(2, '"Tears are words that need to be written"', 'Paulo Coelho'),
-    new Quotes(3, '"Writing books is the closest men ever come to childbearing"', 'Norman Mailer'),
+    new Quotes(1, '"If you are always trying to be normal you will never know how amazing you can be"', 'Maya Angelou', new Date(2020,1,13)),
+    new Quotes(2, '"Tears are words that need to be written"', 'Paulo Coelho', new Date(2020,1,13)),
+    new Quotes(3, '"Writing books is the closest men ever come to childbearing"', 'Norman Mailer', new Date(2020,1,13)),
   ];
   toggleDetails(index){
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
